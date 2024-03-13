ce6160142043aa839ccf45bf69993c0a58a738d5
Benjamin Schäfer <benibschafer@gmail.com>
2024-03-13 12:58:13 +0100

    follow instruction video

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,14 +1,40 @@
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node arrow-functions.js in the terminal
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers1(a, b) {
+  // Code block
+  return a + b;
+}
+let sum = addTwoNumbers1(3, 5);
+console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => {
+  // Code block
+  return a + b;
+}
+let sum2 = addTwoNumbers2(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers3 = (a, b) => a + b;
+let sum3 = addTwoNumbers3(3, 5);
+console.log(sum);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello!');
+
+const sayHello = () => console.log('Hello!');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+    );
+console.log(returnMultipleLines());
---
+++ reduce.js
@@ -0,0 +1,39 @@
+
+/**
+ * To run this file in Gitpod, use the
+ * command node reduce.js in the terminal
+ */
+
+
+// Summing an array of numbers:
+
+
+const teamMembers = [
+    {
+      name: 'Andrew',
+      profession: 'Developer',
+      yrsExperience: 5
+    },
+    {
+      name: 'Ariel',
+      profession: 'Developer',
+      yrsExperience: 7
+    },
+    {
+      name: 'Michael',
+      profession: 'Designer',
+      yrsExperience: 1
+    },
+    {
+      name: 'Kelly',
+      profession: 'Designer',
+      yrsExperience: 3
+    }
+  ];
+
+  // Totaling a specific object property
+  let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0)
+  console.log(totalExperience)
+
+
+  // Grouping by a property, and totaling it too
